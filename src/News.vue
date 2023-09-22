ca54fb986e40419b390b72810aa4b65a13482ed1
Elena Moreva <lenka.moreva@gmail.com>
2023-09-22 10:33:41 +0300

    changed shoes on pinia

---
+++ src/News.vue
@@ -0,0 +1,15 @@
+<template>
+    <h2>Это блок с news</h2>
+    <h2>{{ news.id }}</h2>
+    <h2>{{ news.text }}</h2>
+</template>
+
+<script setup>
+const props = defineProps({
+    news: {
+        type: Object,
+        required: true,
+        default: () => {},
+    }
+});
+</script>
--- src/store.js
+++ src/store.js
@@ -1,11 +1,8 @@
-import { createStore } from "vuex";
+import { defineStore } from "pinia";
 
-const store = createStore({
-    state() {
-        return {
-            count: 1
-        }
-    }
-})
-
-export default store;
+export const useStore = defineStore('MyStore', {
+    state: () => ({
+        count: 7,
+        news: [{id: 1, text: '1News'}, {id: 2, text: '2News'}, {id: 3, text: '3News'}]
+    })
+});
