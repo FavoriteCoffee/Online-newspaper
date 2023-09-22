10313e12289baadfd038264bf7d3fb27ebf48da8
Elena Moreva <lenka.moreva@gmail.com>
2023-09-22 11:37:49 +0300

    added a component with comments

--- src/News.vue
+++ src/News.vue
@@ -1,10 +1,15 @@
 <template>
-    <h2>Это блок с news</h2>
-    <h2>{{ news.id }}</h2>
-    <h2>{{ news.text }}</h2>
+    <div>
+        <h2>Это блок с news</h2>
+        <h3>тут картинка</h3>
+        <h2>{{ news.id }}</h2>
+        <h2>{{ news.text }}</h2>
+    </div>
+    <Comments v-for="comment of news.comments" :key="comment" :comment="comment"/>
 </template>
 
 <script setup>
+import Comments from './Comments.vue';
 const props = defineProps({
     news: {
         type: Object,
