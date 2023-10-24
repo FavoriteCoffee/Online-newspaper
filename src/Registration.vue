7cab3b1be76e9ef5906236d668dd71d2de22c4e2
Elena Moreva <lenka.moreva@gmail.com>
2023-10-24 18:21:14 +0300

    reg updated

--- src/Registration.vue
+++ src/Registration.vue
@@ -5,6 +5,9 @@
     title="Регистрация">
       <v-text-field label="Введите nic"/>
       <v-text-field label="Введите пароль"/>
+      <v-text-field label="Введите имя"/>
+      <v-text-field label="Введите фамилию"/>
+
       <router-link to="/main" class="nav-link">
         <v-btn>OK</v-btn>
       </router-link>
