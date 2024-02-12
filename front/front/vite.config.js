14e5017293ce55a625b6d959f58d9dacec337add
Hippo Maru <hippomaru@ya.ru>
2024-02-12 22:38:52 +0300

    could u find a way to let me down slowly

--- front/front/vite.config.js
+++ front/front/vite.config.js
@@ -9,7 +9,7 @@ import { fileURLToPath, URL } from 'node:url'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue({ 
+    vue({
       template: { transformAssetUrls }
     }),
     // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
@@ -39,3 +39,4 @@ export default defineConfig({
     port: 3000,
   },
 })
+
