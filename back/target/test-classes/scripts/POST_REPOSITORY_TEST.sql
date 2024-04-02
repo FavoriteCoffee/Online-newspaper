ed9581d0972dca34d4ba6f822fcefd0e88683fa2
Elena Moreva <lenka.moreva@gmail.com>
2024-04-02 09:25:09 +0300

    "target" removed

--- back/target/test-classes/scripts/POST_REPOSITORY_TEST.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO public."Post"(id, date)
-VALUES(101, CURRENT_DATE);
-INSERT INTO public."Post"(id, date)
-VALUES(102, CURRENT_DATE);
-INSERT INTO public."Post"(id, date)
-VALUES(103, TO_DATE('07/03/2002', 'DD/MM/YYYY'));
--- back/target/test-classes/scripts/USER_REPOSITORY_TEST.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO public."User"(id, password, user_name, role)
-VALUES (101, 'testpassword', 'Ivan', 'ROLE_USER');
-INSERT INTO public."User"(id, password, user_name, role)
-VALUES (102, 'testpassword', 'Elena', 'ROLE_USER');
-INSERT INTO public."User"(id, password, user_name, role)
-VALUES (103, 'testpassword', 'HippoMaru', 'ROLE_USER');
